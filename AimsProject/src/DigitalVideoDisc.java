375a7fbcc95f4465deacc28862d5858de7ecda7c
Hoang Van Duy <duy.hv204959@sis.hust.edu.vn>
2024-12-04 02:33:52 +0700

    bai3

--- AimsProject/src/DigitalVideoDisc.java
+++ AimsProject/src/DigitalVideoDisc.java
@@ -5,27 +5,30 @@ public class DigitalVideoDisc {
 	private String director;
 	private int length;
 	private float cost;
-	
-	
+
 	@Override
 	public String toString() {
 		return "DVD [title=" + title + ", category=" + category + ", director=" + director + ", length="
 				+ length + ", cost=" + cost + "]";
 	}
+
 	public DigitalVideoDisc(String title) {
 		this.title = title;
 	}
-	public DigitalVideoDisc(String title, String category, float cost ) {
+
+	public DigitalVideoDisc(String title, String category, float cost) {
 		this.title = title;
 		this.category = category;
 		this.cost = cost;
 	}
+
 	public DigitalVideoDisc(String director, String category, String title, float cost) {
 		this.director = director;
 		this.category = category;
 		this.title = title;
 		this.cost = cost;
 	}
+
 	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
 		this.title = title;
 		this.category = category;
@@ -33,20 +36,29 @@ public class DigitalVideoDisc {
 		this.length = length;
 		this.cost = cost;
 	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public String getCategory() {
 		return category;
 	}
+
 	public String getDirector() {
 		return director;
 	}
+
 	public int getLength() {
 		return length;
 	}
+
 	public float getCost() {
 		return cost;
 	}
-	
+
 }
