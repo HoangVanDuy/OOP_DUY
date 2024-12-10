1ccc5a572cf5bafd1d2d5e4c5a1cf548bb3291bf
Hoang Van Duy <duy.hv204959@sis.hust.edu.vn>
2024-12-11 00:17:25 +0700

    lab4

--- AimsProject/src/hust/soict/dsai/test/store/StoreTest.java
+++ AimsProject/src/hust/soict/dsai/test/store/StoreTest.java
@@ -1,25 +1,24 @@
 package hust.soict.dsai.test.store;
 
-import hust.soict.dsai.aims.disc.DigitalVideoDisc;
 import hust.soict.dsai.aims.store.Store;
+import hust.soict.media.DigitalVideoDisc;
 
 public class StoreTest {
 	public static void main(String[] args) {
 		Store store = new Store();
-		DigitalVideoDisc dvd1 = new DigitalVideoDisc(1, "The Lion King", "Animation",
-				"Roger Allers", 87, 19.95f);
+		DigitalVideoDisc dvd1 = new DigitalVideoDisc(1, "The Lion King", "Animation", 87, 19.95f, "");
 
-		DigitalVideoDisc dvd2 = new DigitalVideoDisc(2, "Star Wars", "Science Fiction",
-				"George Lucas", 87, 24.95f);
+		DigitalVideoDisc dvd2 = new DigitalVideoDisc(2, "Star Wars", "Science Fiction", 87, 24.95f, "");
 
-		DigitalVideoDisc dvd3 = new DigitalVideoDisc(3, "Aladin", "Animation", 18.99f);
+		DigitalVideoDisc dvd3 = new DigitalVideoDisc(3, "Aladin", "Animation", 34, 18.99f, "");
+
 		// Add dvds to the store
-		store.addDVD(dvd1, dvd2, dvd3);
+		store.addMedia(dvd3);
 		System.out.println("List after adding :");
 		store.displayItem();
 
 		// Remove dvd3 from store
-		store.removeDVD(dvd3);
+		store.removeMedia(dvd3);
 		System.out.println("List after deletion :");
 		store.displayItem();
 
